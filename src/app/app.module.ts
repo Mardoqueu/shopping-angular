429aa5c3e348a4ba9bf70945739ebf6927982cb7
Mardoqueu Sousa <mdq767@gmail.com>
2023-01-14 15:45:37 -0300

    adding components into the module app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { CartComponent } from './components/cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
