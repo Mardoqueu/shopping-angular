3a4bb5337179acad43642bd76a9e528b64e515ab
Mardoqueu Sousa <mdq767@gmail.com>
2023-01-15 13:34:29 -0300

    consuming api service

--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import {map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  
+  getProduct(){
+    return this.http.get<any>("https://fakestoreapi.com/products/")
+    .pipe(map((response:any)=>{
+        return response;
+    }))
+  }
+
 }
