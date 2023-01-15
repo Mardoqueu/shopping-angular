676fc13ad0aa017367e20ab132db7462d1a5db1f
Mardoqueu Sousa <mdq767@gmail.com>
2023-01-15 13:47:17 -0300

    routing and navigation to carts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './components/cart/cart.component';
+import { ProductsComponent } from './components/products/products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'products', pathMatch: 'full'},
+  {path: 'products', component: ProductsComponent},
+  {path: 'cart', component: CartComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
