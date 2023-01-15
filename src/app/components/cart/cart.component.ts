dee7b43729733079235cb065f80dce206b73f2cd
Mardoqueu Sousa <mdq767@gmail.com>
2023-01-15 15:43:33 -0300

    get total price cart

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CartService } from 'src/app/service/cart.service';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.scss']
 })
-export class CartComponent {
+export class CartComponent implements OnInit {
+  
+  public products : any = [];
+  public grandTotal !: number;
+  constructor(private cartService: CartService){}
+
+  ngOnInit(): void {
+      this.cartService.getProducts()
+      .subscribe(res=>{
+        this.products = res;
+        this.grandTotal = this.cartService.getTotalPrice();
+      })
+  }
 
 }
