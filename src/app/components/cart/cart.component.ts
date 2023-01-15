e65b53f4fc677717b21bc7e23d0063977063041a
Mardoqueu Sousa <mdq767@gmail.com>
2023-01-15 16:30:21 -0300

    fixingname

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -7,23 +7,23 @@ import { CartService } from 'src/app/service/cart.service';
   styleUrls: ['./cart.component.scss']
 })
 export class CartComponent implements OnInit {
-  
+
   public products : any = [];
   public grandTotal !: number;
-  constructor(private cartService: CartService){}
+  constructor(private cartService: CartService){ }
 
   ngOnInit(): void {
-      this.cartService.getProducts()
-      .subscribe(res=>{
-        this.products = res;
-        this.grandTotal = this.cartService.getTotalPrice();
-      })
+    this.cartService.getProducts()
+    .subscribe(res=>{
+      this.products = res;
+      this.grandTotal = this.cartService.getTotalPrice();
+    })
   }
   removeItem(item : any){
     this.cartService.removeCartItem(item);
   }
-
   emptyCart(){
     this.cartService.removeAllCart();
   }
+
 }
